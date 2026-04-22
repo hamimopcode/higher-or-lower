62f932ce7679d4db7a76ea43cd71914da91082db
M K Hamim <mkhamimbd@gmail.com>
2026-04-22 13:57:31 +0600

    Added attempt tracking.

--- js/main.js
+++ js/main.js
@@ -2,22 +2,30 @@ const computerAnswerEl = document.querySelector('.js-computer-answer-el');
 const userInput = document.querySelector('.js-user-input');
 const checkUserInputBtn = document.querySelector('.check-user-guess-btn');
 const generateNumberBtn = document.querySelector('.js-generate-number-btn');
-const computerMessage = document.querySelector('.js-computer-message-el');
+const computerMessageEl = document.querySelector('.js-computer-message-el');
 const restartGameBtn = document.querySelector('.js-restart-game-btn');
+const attemptsEl = document.querySelector('.js-attempts-el');
 
 let generatedNumber;
+let attempts = 0;
 
 generateNumberBtn.addEventListener('click', () => {
     generatedNumber = generateNumber();
-    computerMessage.textContent = 'Generated a new number!';
+    computerMessageEl.textContent = 'Generated a new number!';
 });
 
-checkUserInputBtn.addEventListener('click', checkUserGuess);
+checkUserInputBtn.addEventListener('click', () => {
+    checkUserGuess();
+    updateAttemptsCount();
+});
 userInput.addEventListener('keydown', () => {
     if (event.key === 'Enter') {
-        checkUserGuess()
+        checkUserGuess();
+        updateAttemptsCount();
     }
 });
+
+
 
 restartGameBtn.addEventListener('click', restartGame);
 
@@ -29,22 +37,30 @@ function checkUserGuess() {
     const userGuess = Number(userInput.value);
 
     if (!generatedNumber) {
-        computerMessage.textContent = 'Generate a number first';
-    } 
-
-    if (userGuess === generatedNumber) {
+        computerMessageEl.textContent = 'Generate a number first.';
+    } else if (userInput.value === '') {
+        computerAnswerEl.textContent = 'Enter a valid number.'
+    } else if (userGuess === generatedNumber) {
         computerAnswerEl.textContent = 'Correct Guess!';
+        attempts += 1;
     } else if (userGuess > generatedNumber) {
         computerAnswerEl.textContent = 'LOWER';
+        attempts += 1;
     } else if (userGuess < generatedNumber) {
         computerAnswerEl.textContent = 'HIGHER';
+        attempts += 1;
     }
-
     userInput.value = '';
 }
 
 function restartGame() {
     generatedNumber = null;
     computerAnswerEl.textContent = '';
-    computerMessage.textContent = '';
+    computerMessageEl.textContent = '';
 }
+
+function updateAttemptsCount() {
+    attemptsEl.textContent = `Attempts: ${attempts}`;
+}
+
+updateAttemptsCount();
